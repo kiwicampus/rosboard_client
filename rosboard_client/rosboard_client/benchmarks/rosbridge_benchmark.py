641c2e8cae8af5772682451f3c1dcebc2845d221
NicolasRochaPacheco <n.nicolas98@hotmail.com>
2022-08-16 17:00:31 +0000

    [DOC]: Added GNUv3 licence.

--- rosboard_client/rosboard_client/benchmarks/rosbridge_benchmark.py
+++ rosboard_client/rosboard_client/benchmarks/rosbridge_benchmark.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+"""
+License:
+    This file is part of Rosboard Client.
+
+    Rosboard Client is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    Rosboard Client is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with Rosboard Client. If not, see <https://www.gnu.org/licenses/>.
+
+Code Information:
+    Code Information:
+    Maintainer: Eng. Pedro Alejandro Gonzalez B
+	Mail: pedro@kiwibot.com
+"""
+
+# =============================================================================
 from threading import Thread
 from grpc import Compression
 import yaml
--- rosboard_client/rosboard_client/rosboard_client_gui.py
+++ rosboard_client/rosboard_client/rosboard_client_gui.py
@@ -2,6 +2,22 @@
 # -*- coding: utf-8 -*-
 # =============================================================================
 """
+License:
+    This file is part of Rosboard Client.
+
+    Rosboard Client is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    Rosboard Client is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with Rosboard Client. If not, see <https://www.gnu.org/licenses/>.
+
 Code Information:
     Code Information: PyQt5 user interface for the rosboard client.
     Maintainer: Nicolas Rocha Pacheco
